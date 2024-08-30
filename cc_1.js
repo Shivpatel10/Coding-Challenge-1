fe749e31571e4158b89bd90ca1a72b4a6d9dbc9e
Shiv Patel <shivpatel0204@gmail.com>
2024-08-30 15:52:01 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "John Doe";
+const employeeID = 813;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
